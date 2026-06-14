ddaa011b1b819f30d6195d27e6d8010293d03096
wardahnabilah <119636138+wardahnabilah@users.noreply.github.com>
2026-06-14 23:51:13 +0700

    fix: add data response for login api

--- app/Http/Controllers/Api/V1/UserController.php
+++ app/Http/Controllers/Api/V1/UserController.php
@@ -42,6 +42,7 @@ class UserController extends Controller
                 'accessToken'  => $token,
                 'user_id'      => $user->id,
                 'name'         => $user->name,
+                'email'        => $user->email,
                 'role'         => $user->role,
             ]
         ]);
